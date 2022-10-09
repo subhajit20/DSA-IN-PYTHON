f093dd06b94ddfd9872840d8277fd179418c662a
Subhajit Ghosh <subhajitstudent8@gmail.com>
2022-10-09 09:25:09 +0530

    Fixed some issue

--- Priority_Queue.py
+++ Priority_Queue.py
@@ -13,13 +13,12 @@ class Priority_Queue:
     def enqueue(self,data,priority):
         if len(self.arr) == 0:
             new_priority_queue = Item(data,priority)
-            self.size = self.size + 1
             self.arr.append(new_priority_queue)
+            self.size = self.size + 1
         elif len(self.arr) > 0:
             new_priority_queue = Item(data,priority)
             self.arr.append(new_priority_queue)
-            for i in range(0,len(self.arr)):
-                self.size = self.size + i
+            self.size = self.size + 1
 
     def Print_Queue(self):
         if len(self.arr) == 0:
@@ -55,21 +54,22 @@ class Priority_Queue:
             queue = self.arr
             if len(self.arr)-1 == ind:
                 del self.arr[ind]
-                for i in range(0,len(self.arr)):
-                    self.size = self.size + i
+                self.size = self.size - 1
             else:
                 for i in range(ind,len(self.arr)):
                     next = i + 1
                     if next <= len(self.arr)-1:
                         self.arr[i] = self.arr[next]
                         del queue[next]
-                for n in range(0,len(self.arr)):
-                    self.size = self.size + n
+                self.size = self.size - 1
 new_queue = Priority_Queue()
 
 new_queue.enqueue(10,70)
 new_queue.enqueue(50,30)
 new_queue.enqueue(20,40)
+new_queue.dequeu()
+new_queue.dequeu()
+new_queue.dequeu()
 new_queue.Print_Queue()
 
 print(new_queue.size)
