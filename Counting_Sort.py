17234b687d8db6462676fbb7c423a9ee1b676461
Subhajit Ghosh <subhajitstudent8@gmail.com>
2022-10-10 11:53:37 +0530

    Implement counting sort

---
+++ Counting_Sort.py
@@ -0,0 +1,34 @@
+# Counting sort algorithm
+
+def Counting_Sort(arr):
+    empty_arr = []
+    
+    max = arr[0]
+    for i in range(0,len(arr)):
+        if max < arr[i]:
+            max = arr[i]
+            empty_arr = [0] * (max+1)
+
+    for n in range(0,len(arr)):
+        m = 1
+        if empty_arr[arr[n]] == 0:
+            empty_arr[arr[n]] = m
+        elif empty_arr[arr[n]] > 0:
+            m = empty_arr[arr[n]] + 1
+            empty_arr[arr[n]] = m
+            
+    n = 0
+    for i in range(0,len(empty_arr)):
+        if empty_arr[i] != 0:
+            while n <= len(arr)-1 and empty_arr[i] != 0:
+                if empty_arr[i] == 0:
+                    break
+                arr[n] = i
+                n = n + 1
+                empty_arr[i] = empty_arr[i] - 1
+
+    return arr
+
+arr = [6,2,8,4]
+newarr = Counting_Sort(arr)
+print(arr)
