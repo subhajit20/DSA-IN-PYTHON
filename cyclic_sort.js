8c0856a1bc50928d90383a6f2c24b208a2908869
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-11-02 20:31:19 +0530

    Modified cyclic sort algo

--- cyclic_sort.js
+++ cyclic_sort.js
@@ -1,28 +1,26 @@
-let unsortedArray = [5,4,3,2,1,10,6];
+let uArray = [5, 4, 3, 2, 1, 0];
 
-function cyclic_sort(unsortedArray){
-  let i = 0;
-  while (i <= unsortedArray.length - 1) {
-    let currentValue = unsortedArray[i];
-    let currentIndex = i + 1;
-    if (currentValue !== currentIndex) {
-      if (currentValue > unsortedArray.length - 1) {
-        i++;
-      } else {
-        swap(unsortedArray,currentIndex,currentValue)
-      }
-    } else {
-      i++;
+
+function cyclic_sort(arr) {
+    let i = 0;
+
+    while (i <= arr.length - 1) {
+        let correct = arr[i];
+        let currentIndex = i;
+
+        if (arr[i] !== arr[correct] && correct <= arr.length - 1) {
+            swap(arr, currentIndex, correct);
+        } else {
+            i++;
+        }
     }
-  }
 }
 
-function swap(arr,currentIndex,currentValue){
-  let temp = arr[currentValue-1];
-  arr[currentValue-1] = currentValue;
-  arr[currentIndex - 1] = temp;
+const swap = (arr, currentIndex, currentValue) => {
+    let temp = arr[currentValue];
+    arr[currentValue] = currentValue;
+    arr[currentIndex] = temp
 }
 
-cyclic_sort(unsortedArray);
-
-console.log(unsortedArray);
+cyclic_sort(uArray)
+console.log(uArray)
